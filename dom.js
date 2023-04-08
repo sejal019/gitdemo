3e5a4131ac3d2fef75ebb17a4eb1e3df6e1955dd
sejal019 <jainsejal06@gmail.com>
2023-04-08 16:50:40 +0530

    adding second file

---
+++ dom.js
@@ -0,0 +1,5 @@
+
+var headerTitle=document.getElementById('main-header');
+headerTitle.style.border='solid 5px #000';
+document.getElementById('add').style.color='green';
+document.getElementById('add').style.fontSize='300%';
